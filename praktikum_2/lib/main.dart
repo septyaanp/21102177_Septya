e6bf9cf816ce11785b776909af4be09b76340c3e
septyaanp <septyaandiniputri@gmail.com>
2025-03-03 13:47:57 +0700

    listview

--- praktikum_2/lib/main.dart
+++ praktikum_2/lib/main.dart
@@ -18,42 +18,23 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(title: const Text('ListView.builder Example')),
         body: Padding(
             padding: const EdgeInsets.all(10.0),
-            child: GridView.count(
-              primary: false,
-              padding: const EdgeInsets.all(20),
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-              crossAxisCount: 2,
+            child: ListView(
+              padding: const EdgeInsets.all(8),
               children: <Widget>[
                 Container(
-                  padding: const EdgeInsets.all(8),
-                  child: const Text("He'd have you all unravel at the"),
-                  color: Colors.teal[100],
+                  height: 50,
+                  color: Colors.amber[600],
+                  child: const Center(child: Text('Entry A')),
                 ),
                 Container(
-                  padding: const EdgeInsets.all(8),
-                  child: const Text('Heed not the rabble'),
-                  color: Colors.teal[200],
+                  height: 50,
+                  color: Colors.amber[500],
+                  child: const Center(child: Text('Entry B')),
                 ),
                 Container(
-                  padding: const EdgeInsets.all(8),
-                  child: const Text('Sound of screams but the'),
-                  color: Colors.teal[300],
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: const Text('Who scream'),
-                  color: Colors.teal[400],
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: const Text('Revolution is coming...'),
-                  color: Colors.teal[500],
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: const Text('Revolution, they...'),
-                  color: Colors.teal[600],
+                  height: 50,
+                  color: Colors.amber[100],
+                  child: const Center(child: Text('Entry C')),
                 ),
               ],
             )),
