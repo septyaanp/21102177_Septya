bf1556cb45169e0d95bc44e139493b64452a311e
septyaanp <septyaandiniputri@gmail.com>
2025-03-03 13:54:08 +0700

    listview.builder

--- praktikum_2/lib/main.dart
+++ praktikum_2/lib/main.dart
@@ -18,16 +18,19 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(title: const Text('ListView.builder Example')),
         body: Padding(
             padding: const EdgeInsets.all(10.0),
-            child: ListView.builder(
-                padding: const EdgeInsets.all(8),
-                itemCount: entries.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    height: 50,
-                    color: Colors.amber[colorCodes[index]],
-                    child: Center(child: Text('Entry ${entries[index]}')),
-                  );
-                })),
+            child: ListView.separated(
+              padding: const EdgeInsets.all(8),
+              itemCount: entries.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  height: 50,
+                  color: Colors.amber[colorCodes[index]],
+                  child: Center(child: Text('Entry ${entries[index]}')),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(),
+            )),
       ),
     );
   }
