1aefa1200504718b680a8067c1032c7dd1d624c1
septyaanp <septyaandiniputri@gmail.com>
2025-05-19 18:08:18 +0700

    laravel(lanjutan)

--- praktikum_10/car_laravel/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\CarController;
-use Illuminate\Support\Facades\Route;
- 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::resource('/car', CarController::class);
